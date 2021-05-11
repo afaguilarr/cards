ed810e7da040b0421dad924eb5d1a5511b096cd0
Andres Aguilar <aaguilar@MacBook-Pro-de-IT.local>
2021-05-11 15:48:03 -0500

    Git Test

--- main.go
+++ main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-    "cards/deck"
-    "io/ioutil"
-    "log"
-    "os"
+	"cards/deck"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
-    setLogsState(os.Args)
-    d := deck.NewDeck()
-    hand, d := d.GetHandAndRemainingDeck(7)
-    hand.SaveToFile()
-    d.SaveToFile()
-    d = deck.LoadDeckFromFile()
-    hand = deck.LoadHandFromFile()
+	setLogsState(os.Args)
+	d := deck.NewDeck()
+	hand, d := d.GetHandAndRemainingDeck(7)
+	hand.SaveToFile()
+	d.SaveToFile()
+	d = deck.LoadDeckFromFile()
+	hand = deck.LoadHandFromFile()
+	fmt.Println("HOLIIII")
 }
 
 func setLogsState(osArgs []string) {
-    areLogsEnabled := ""
-    if len(osArgs) > 1 {areLogsEnabled = osArgs[1]}
-    if areLogsEnabled == "no-logs" {log.SetOutput(ioutil.Discard)}
+	areLogsEnabled := ""
+	if len(osArgs) > 1 {
+		areLogsEnabled = osArgs[1]
+	}
+	if areLogsEnabled == "no-logs" {
+		log.SetOutput(ioutil.Discard)
+	}
 }
